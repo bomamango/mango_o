f256dda62c922d1cfb7e68b2de24ecdc11646ca2
radmin <minlend@msn.com>
2016-09-05 19:41:35 -0500

    Modified templates to display posts from database.

--- boma/urls.py
+++ boma/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^mango_o/', include('mango_o.urls')),
+    url(r'', include('mango_o.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
 ]
