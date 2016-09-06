4cd770f4b4f9accfe61f7f4341eca5fd331c4e9f
radmin <minlend@msn.com>
2016-09-05 23:21:27 -0500

    Added views and template for detailed blog post as well as CSS for the site.

--- mango_o/views.py
+++ mango_o/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Me'h yega be'h Oooh... Index djem ini'h.")
+    return HttpResponse("Me'h yega be'h Oooh... Index djem ini'h. Allez plutot a --> <a href=>http://bomamango.pythonanywhere.com/blog/</a> --> copy/paste le lien ne waka pas ankor. Yia koi?")
